83f9b75d875bd5f33a9874b1fc3b86fd5664563e
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2021-10-17 20:02:16 -0300

    Add touch events for cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,17 @@ const Card = (props) => {
   const { dataID, image: src, text, onClick } = props;
 
   return (
-    <S.Card className="card" data-id={dataID} onClick={(e) => onClick(e)}>
+    <S.Card
+      className="card"
+      data-id={dataID}
+      onClick={(e) => onClick(e)}
+      onTouchStart={(e) => {
+        e.target.style.boxShadow = '0px 4px 3px 0px #f46565';
+      }}
+      onTouchEnd={(e) => {
+        e.target.style.boxShadow = '0px 4px 4px 1px rgba(0, 0, 0, 0.25)';
+      }}
+    >
       <Image src={src} alt={text} />
       <S.Legend>{text}</S.Legend>
     </S.Card>
