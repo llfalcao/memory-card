33c3f30308f8e99d22ee5d8a75ac06aa00756902
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2021-10-17 00:13:24 -0300

    Add card info

---
+++ src/assets/cards/cards.js
@@ -0,0 +1,96 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const cards = [
+  {
+    id: uuidv4(),
+    src: '../images/01.jpg',
+    text: 'Bronn',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/02.jpg',
+    text: 'Sansa Stark',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/03.jpg',
+    text: 'Lord Varys',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/04.jpg',
+    text: 'Cersei Lannister',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/05.jpg',
+    text: 'Jaime Lannister',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/06.jpg',
+    text: 'Jon Snow',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/07.jpg',
+    text: 'Tyrion Lannister',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/08.jpg',
+    text: 'Davos Seaworth',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/09.jpg',
+    text: 'Brienne of Tarth',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/10.jpg',
+    text: 'Sandor Clegane',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/11.jpg',
+    text: 'Daenerys Targaryen',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/12.jpg',
+    text: 'Bran Stark',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/13.jpg',
+    text: 'Arya Stark',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/14.jpg',
+    text: 'Joffrey Baratheon',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/15.jpg',
+    text: 'Theon Greyjoy',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/16.jpg',
+    text: 'Night King',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/17.jpg',
+    text: 'Tormund Giantsbane ',
+  },
+  {
+    id: uuidv4(),
+    src: '../images/18.jpg',
+    text: 'Samwell Tarly',
+  },
+];
+
+export default cards;
