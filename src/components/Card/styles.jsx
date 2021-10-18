0c9e4621224e1330c5ccfc5f2e3f163ecc3c666a
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2021-10-18 03:52:42 -0300

    Change card style

--- src/components/Card/styles.jsx
+++ src/components/Card/styles.jsx
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 export const Card = styled.div`
   width: 180px;
   height: 205px;
-  background: #f1f1f1;
-  box-shadow: 0px 4px 5px 2px rgb(0, 0, 0, 0.1);
-  border-radius: 25px;
+  background: linear-gradient(to bottom, #ece9ff, #f1f1f100);
+  box-shadow: 0px 2px 10px 2px rgb(0, 0, 0, 0.1);
+  border-radius: 20px;
   cursor: pointer;
   transition: transform 0.1s ease-out;
   user-select: none;
 
   &:hover {
-    box-shadow: 0px 4px 3px 0px #f4656590;
+    background: linear-gradient(to bottom, #ece9ff, #f1f1f18b);
   }
 
   &:active {
