787f757493f00af0dc5e2df3a478233893d01404
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2021-10-17 21:48:40 -0300

    Update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,11 @@ import * as S from './styles';
 const Header = () => {
   return (
     <S.Header id="header">
-      <S.Title>Memory Card</S.Title>
-      <S.Subtitle>
+      <S.Title>Memory Game</S.Title>
+      <S.Subtitle>Game of Thrones Edition</S.Subtitle>
+      <S.Paragraph>
         Click on a card to increase your score, but not more than once!
-      </S.Subtitle>
+      </S.Paragraph>
     </S.Header>
   );
 };
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -8,11 +8,18 @@ export const Title = styled.h1`
   font: bold calc(24px + 2vmin) Manrope;
   color: #f46565;
   text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.3);
-  margin: 1rem 0;
+  margin: 1rem 0 0;
 `;
 
-export const Subtitle = styled.p`
-  font: normal calc(5px + 2vmin) Open Sans;
-  color: #303030;
+export const Subtitle = styled.h2`
+  font: bold calc(4px + 2vmin) Manrope;
+  color: #00000095;
+`;
+
+export const Paragraph = styled.p`
+  font: 500 calc(4px + 2vmin) SF Pro Display, -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    sans-serif;
+  color: #444;
   margin: 1rem 0 1.5rem;
 `;
